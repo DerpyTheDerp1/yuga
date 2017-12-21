01d86996341dc162c72522bd38de0a9340da27af
Striker <34070194+Strikerrr@users.noreply.github.com>
2017-12-21 13:10:12 +0000

    Fuck

--- events/message.js
+++ events/message.js
@@ -62,7 +62,7 @@ exports.run = async(client, msg) => {
     }
 
     //Prefix checker #3: Mentions
-    if (msg.mentions.users.first().has(client.user.id, {
+    if (msg.mentions.users.has(client.user.id, {
             ignoreEveryone: true
         })) {
 
