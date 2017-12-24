5f02ba6c6a045b90cf73dcb9644a0ba0dc4ce209
Striker <zzstrikerzzyt@gmail.com>
2017-12-24 21:01:21 +0000

    Better

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -1,20 +1,15 @@
 const prefixes = require('../events/ready.js').prefixes;
 
-exports.run = (client, msg, args) => {
+exports.run = async (client, msg, args) => {
     const prefix = args.join(' ');
-    prefixes.sync().then(() => {
-        prefixes.update({
+    prefixes.sync().then(async () => {
+       await prefixes.update({
                 prefix: prefix
             }, {
                 where: {
                    GuildID: msg.guild.id
                 }
-            })
-            .success(() => {
-                msg.reply(`Set prefix for this server to: ${prefix}`);
-            })
-            .error(err => {
-                msg.reply(`Failure, error:\n\n\`\`\`${err}\`\`\``)
             });
+        msg.reply(`Set prefix for this server to: ${prefix}`);
     });
 };
