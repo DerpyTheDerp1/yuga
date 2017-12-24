e08ba105da22c247f11a75c53c60ea7b6884ca04
Striker <zzstrikerzzyt@gmail.com>
2017-12-24 20:19:09 +0000

    Updated ping command for far more accurate results!

--- commands/ping.js
+++ commands/ping.js
@@ -3,13 +3,10 @@ const db = require('../db/db.json');
 
 exports.run = async (client, msg) => {
     const author = msg.author.tag;
-    console.log('Pinging!');
-
-    const startTime = Date.now();
-
-    const message = await msg.channel.send('Pinging');
-   const endTime = Date.now();
-    const ping = Math.round(endTime - startTime);
+    console.log('Pinging');
+    const m1 = await msg.channel.send('Pinging');
+    const m2 = await m1.edit('Please wait...');
+    const ping = Math.round(m2.createTimestamp - m1.createTimestamp);
     const roundedping = ping / 1000;
     const pingembed = new Discord.MessageEmbed()
         .addField('PING', `**${ping}** milliseconds\n**${roundedping}** seconds`)
