a8b4614102253784b03111875b810882ecd54253
Striker <zzstrikerzzyt@gmail.com>
2017-12-24 20:58:11 +0000

    Personally, I prefer the OLD ping.

--- commands/ping.js
+++ commands/ping.js
@@ -3,17 +3,20 @@ const db = require('../db/db.json');
 
 exports.run = async (client, msg) => {
     const author = msg.author.tag;
-    console.log('Pinging');
-    const m1 = await msg.channel.send('Pinging');
-    const m2 = await m1.edit('Please wait...');
-    const ping = Math.round(m2.createTimestamp - m1.createTimestamp);
+    console.log('Pinging!');
+
+    const startTime = Date.now();
+
+    const message = await msg.channel.send('Pinging');
+   const endTime = Date.now();
+    const ping = Math.round(endTime - startTime);
     const roundedping = ping / 1000;
     const pingembed = new Discord.MessageEmbed()
         .addField('PING', `**${ping}** milliseconds\n**${roundedping}** seconds`)
         .setTimestamp()
         .setThumbnail(client.user.avatarURL())
         .setColor(`${db.color}`);
-    m1.edit({
+    message.edit({
         embed: pingembed
     });
     console.log('Pinged by ' + author);
