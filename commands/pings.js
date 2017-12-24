f14c2335ae4e4d32155313c05b3dd90b6b8e8a8b
Striker <zzstrikerzzyt@gmail.com>
2017-12-24 21:42:26 +0000

    manipulation of the array

--- commands/pings.js
+++ commands/pings.js
@@ -1,11 +1,13 @@
 const recentPings = require('./ping.js').recentPings;
 const Discord = require('discord.js');
+let pings = recentPings.toString();
+pings = pings.split('\n');
 
 exports.run = (client, msg) => {
     const recentPingEmbed = new Discord.MessageEmbed()
         .setAuthor('Yuga')
         .setTitle('Recent Pings')
-        .setDescription(`These are Yuga's most recent pings!\n\n\n${recentPings.split('\n')}'`)
+        .setDescription(`These are Yuga's most recent pings!\n\n\n${pings}}'`)
         .setTimestamp()
         .setThumbnail(client.user.avatarURL());
 
