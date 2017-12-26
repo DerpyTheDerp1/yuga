f36f100636f6408beb652643f2f038c053629190
Striker <34070194+Strikerrr@users.noreply.github.com>
2017-12-26 22:50:19 +0000

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,16 +1,6 @@
 const superagent = require('superagent');
 
 exports.run = async(client) => {
- if (process.env.DBENABLED == 'no') return;
-else {
- superagent.post('https://discordbots.org/api/bots/stats')
-    .set('Authorization', process.env.DBTOKEN)
-    .send({
-      server_count: client.guilds && client.guilds.size ? client.guilds.size : (client.Guilds ? client.Guilds.size : Object.keys(client.Servers).length)
-    })
-    .then(() => console.log('Updated discordbots.org stats!'))
-    .catch(err => console.error(`Error updating discordbots.org stats: ${err.body} || ${err}`));
-}
   const guilds = client.guilds.size;
   client.user.setActivity(`for y!help | ${guilds} servers`, {
     type: 'WATCHING'
@@ -22,5 +12,14 @@ else {
     client.channels.get('308278703283240960').send(`Yuga's recent pings are: ${client.pings}, with an average of ${client.ping}. (API response time for Discord)`),
       1200000;
   });
-
+ if (process.env.DBENABLED == 'no') return;
+else {
+ superagent.post('https://discordbots.org/api/bots/stats')
+    .set('Authorization', process.env.DBTOKEN)
+    .send({
+      server_count: client.guilds && client.guilds.size ? client.guilds.size : (client.Guilds ? client.Guilds.size : Object.keys(client.Servers).length)
+    })
+    .then(() => console.log('Updated discordbots.org stats!'))
+    .catch(err => console.error(`Error updating discordbots.org stats: ${err.body} || ${err}`));
+ }
 };
