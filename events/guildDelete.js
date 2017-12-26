4cc17555650f4ed57ee9d3248e69cdc43fb42a0e
Striker <34070194+Strikerrr@users.noreply.github.com>
2017-12-26 22:49:42 +0000

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,15 +1,17 @@
 const superagent = require('superagent')
 
 exports.run = async (client, guild) => {
- superagent.post('https://discordbots.org/api/bots/stats')       
-   .set('Authorization', process.env.DBTOKEN)       
-   .send({ server_count: client.guilds && client.guilds.size ? client.guilds.size : (client.Guilds ? client.Guilds.size : Object.keys(client.Servers).length) })       
-   .then(() => console.log('Updated discordbots.org stats!'))       
-   .catch(err => console.error(`Error updating discordbots.org stats: ${err.body} || ${err}`))
-
   await client.user.setActivity(`for y!help | ${client.guilds.size} servers`, {
     type: 'WATCHING'
   });
   guild.owner.send('Hi! We see you kicked our bot. Could you explain briefly to Striker#7250 why, and we can consider improvements and such. Thanks!');
   client.channels.get('308649578318528513').send(`Kick from: ${guild.name}\nOwner: ${guild.owner.user.username}#${guild.owner.user.discriminator} (ID: ${guild.owner.id})`);
+ if (process.env.DBENABLED == 'no') return;
+ else {
+ superagent.post('https://discordbots.org/api/bots/stats')       
+   .set('Authorization', process.env.DBTOKEN)       
+   .send({ server_count: client.guilds && client.guilds.size ? client.guilds.size : (client.Guilds ? client.Guilds.size : Object.keys(client.Servers).length) })       
+   .then(() => console.log('Updated discordbots.org stats!'))       
+   .catch(err => console.error(`Error updating discordbots.org stats: ${err.body} || ${err}`))
+ }
 };
