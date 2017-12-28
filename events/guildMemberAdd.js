f5f7557a73d5b516a42d98d31c27a4d02b79e668
Striker <zzstrikerzzyt@gmail.com>
2017-12-28 21:08:30 +0000

    Hmmm

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,6 @@
 exports.run = (client, member) => {
     const guild = member.guild;
-    const channel = member.guild.channels.find('name', 'yuga-welcome');
+    const channel = guild.channels.find('name', 'yuga-welcome');
     if (!channel) return;
     channel.send(`Welcome to ${guild.name}, ${member}!`);
 };
