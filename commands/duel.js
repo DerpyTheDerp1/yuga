3ed4e0f0471a8f4e83f42c99075eb76a02fae9c0
Striker <zzstrikerzzyt@gmail.com>
2017-12-28 21:31:02 +0000

    Added duel command

---
+++ commands/duel.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+
+exports.run = async(client, msg) => {
+    const duelhelp = new Discord.MessageEmbed()
+        .setTitle('Duel Usage')
+        .setAuthor('Yuga')
+        .setColor(0x32CD32)
+        .addField('About', 'Fight with another user!', false)
+        .addField('Usage', 'y!duel <user mention>', false)
+        .addField('Perms required', 'None')
+        .setThumbnail(client.user.avatarURL())
+        .setTimestamp();
+    const user1 = msg.author;
+    const user2 = msg.mentions.users.first();
+    if (!user2) return msg.channel.send({
+        embed: duelhelp
+    });
+    const users = [].push(user1, user2);
+    const winner = users[Math.floor(Math.random() * users.length)];
+    message = await msg.channel.send(`${user1} is dueling ${user2}!`);
+    await message.edit('⚔ Dueling');
+    await message.edit('⚔ Dueling.');
+    await message.edit('⚔ Dueling..');
+    await message.edit('⚔ Dueling...');
+    await message.edit(`${winner} has won!`);
+};
