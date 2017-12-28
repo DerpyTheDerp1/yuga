785e9a5d1f1b3ed8d809a0da12668442acdca883
Striker <zzstrikerzzyt@gmail.com>
2017-12-28 21:04:21 +0000

    Fixed welcome messages

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,5 @@
 exports.run = (client, member) => {
+    const guild = member.guild;
     const channel = member.guild.channels.find('name', 'yuga-welcome');
     if (!channel) return;
     channel.send(`Welcome to ${guild.name}, ${member}!`);
