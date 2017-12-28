0f0ce8cbd21b5ddad7a20fe4ab2fc131e3ebb26a
Striker <zzstrikerzzyt@gmail.com>
2017-12-28 20:46:00 +0000

    Modulizing error logging

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,19 +1,9 @@
 const prefix = 'y!';
 const Discord = require('discord.js');
 const color = require('../db/db.json').color;
+const error = require('../yuga.js').error;
 
 exports.run = async(client, old, msg) => {
-    function error(err) {
-        const errorembed = new Discord.MessageEmbed()
-            .setColor(color)
-            .setTitle('New Error Caught!')
-            .setTimestamp()
-            .setDescription(`\`\`\`xl\n${err.stack}\`\`\``);
-        client.channels.get('385485532458778626').send({
-            embed: errorembed
-        });
-    }
-
     let args = msg.content.split(' ').slice(1);
     let command = msg.content.split(' ')[0];
     command = command.slice(prefix.length);
