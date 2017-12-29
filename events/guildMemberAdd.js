6dff0a3c9fcf97eafa0a6278384c6dc6e1fe0102
Striker <zzstrikerzzyt@gmail.com>
2017-12-29 18:45:38 +0000

    Autoroling?

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,8 @@
 exports.run = (client, member) => {
     const guild = member.guild;
+    const memberRole = guild.roles.find('name', 'Member');
+    if (memberRole) member.addRole(member);
+    if (!memberRole) return undefined;
     const channel = guild.channels.find('name', 'yuga-welcome');
     if (!channel) return;
     channel.send(`Welcome to ${guild.name}, ${member}!`);
