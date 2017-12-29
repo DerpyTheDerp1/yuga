d8481cb66d26d03813c7114a0acc17c09e6704fd
Striker <zzstrikerzzyt@gmail.com>
2017-12-29 16:57:19 +0000

    Now I'll know if testing butt is online

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,10 @@
 const superagent = require('superagent');
-let prefix = ''
+let prefix = '';
 exports.run = async(client) => {
   const guilds = client.guilds.size;
-  
-if (client.user.username == 'Yuga!') prefix = 'y!'
-if (client.user.username == 'Yuga Testing') prefix = 'yt!'
+
+  if (client.user.username == 'Yuga!') prefix = 'y!';
+  if (client.user.username == 'Yuga Testing') prefix = 'yt!';
 
   client.user.setActivity(`for ${prefix}help | ${guilds} servers`, {
     type: 'WATCHING'
@@ -12,18 +12,18 @@ if (client.user.username == 'Yuga Testing') prefix = 'yt!'
   console.log('Yuga is connected to the Discord WebSocket');
   const channel = client.channels.get('308278703283240960');
   if (channel) channel.send('Yuga is now online!');
-  setTimeout(function () {
-    client.channels.get('308278703283240960').send(`Yuga's recent pings are: ${client.pings}, with an average of ${client.ping}. (API response time for Discord)`),
-      1200000;
-  });
- if (process.env.DBENABLED == 'no') return;
-else {
- superagent.post('https://discordbots.org/api/bots/stats')
-    .set('Authorization', process.env.DBTOKEN)
-    .send({
-      server_count: client.guilds && client.guilds.size ? client.guilds.size : (client.Guilds ? client.Guilds.size : Object.keys(client.Servers).length)
-    })
-    .then(() => console.log('Updated discordbots.org stats!'))
-    .catch(err => console.error(`Error updating discordbots.org stats: ${err.body} || ${err}`));
- }
+  if (!channel) {
+    const otherChannel = client.channels.get('395355797716074509');
+    if (otherChannel) otherChannel.send('Yuga is now online!');
+  }
+  if (process.env.DBENABLED == 'no') return;
+  else {
+    superagent.post('https://discordbots.org/api/bots/stats')
+      .set('Authorization', process.env.DBTOKEN)
+      .send({
+        server_count: client.guilds && client.guilds.size ? client.guilds.size : (client.Guilds ? client.Guilds.size : Object.keys(client.Servers).length)
+      })
+      .then(() => console.log('Updated discordbots.org stats!'))
+      .catch(err => console.error(`Error updating discordbots.org stats: ${err.body} || ${err}`));
+  }
 };
