dcffddc4dc6f009088c14847b30ece04c5491772
Striker <zzstrikerzzyt@gmail.com>
2017-12-30 13:18:13 +0000

    Update help.js

--- commands/Main/help.js
+++ commands/Main/help.js
@@ -3,12 +3,32 @@ const fs = require('fs');
 const path = require('path');
 
 exports.run = async(client, msg, args) => {
+  function findCommand(cmd) {
+    try {
+      const CommandsFolder = fs.readdirSync('../commands');
+      for (const group of CommandsFolder) {
+        try {
+          commands = fs.readdirSync('../commands/' + group);
+          for (const command of commands) {
+            if (command.slice(0, -3) === cmd) {
+              require('../commands/' + group + '/' + command);
+            }
+          }
+        } catch (err) {
+          console.error;
+        }
+      }
+    } catch (err) {
+      console.error;
+    }
+  }
+
   let prefix = '';
   if (client.user.username == 'Yuga!') prefix = 'y!';
   if (client.user.username == 'Yuga Testing') prefix == 'yt!';
   const cmdName = args.join(' ');
   if (cmdName) {
-    const cmdHelp = require(`./${cmdName}.js`)['help'];
+    const cmdHelp = findCommand(cmdName)['help'];
     const help = new Discord.MessageEmbed()
       .setAuthor(client.user.username)
       .setTitle(`__Help for ${cmdHelp.name}__`)
@@ -22,13 +42,21 @@ exports.run = async(client, msg, args) => {
       embed: help
     });
   } else {
-    const commands = fs.readdirSync(__dirname).map(file => path.basename(file, path.extname(file)));
+    const ExclusiveCommands = fs.readdirSync('../Exclusive').map(file => path.basename(file, path.extname(file)));
+    const FunCommands = fs.readdirSync('../Fun').map(file => path.basename(file, path.extname(file)));
+    const MainCommands = fs.readdirSync('../Main').map(file => path.basename(file, path.extname(file)));
+    const ModerationCommands = fs.readdirSync('../Moderation').map(file => path.basename(file, path.extname(file)));
+    const UtilityCommands = fs.readdirSync('../Utility').map(file => path.basename(file, path.extname(file)));
     const embed = new Discord.MessageEmbed()
       .setTitle('Help Command')
       .setAuthor(client.user.username)
       .setColor('#32CD32')
       .setDescription('Yuga is a Discord Bot with many features!\nIf you wish to know the features, look down below!')
-      .addField('Commands', commands, true)
+      .addField('Main Commands', MainCommands, true)
+      .addField('Fun Commands', FunCommands, true)
+      .addField('Moderation Commands', ModerationCommands, true)
+      .addField('Utility Commands', UtilityCommands, true)
+      .addField('Exclusive Commands', ExclusiveCommands, true)
       .addField('More info', `To find out extensive usage per command, use ${prefix}help <command name>.\nThis will tell you the command description, usage, and what perms you need to run it. `, true)
       .setThumbnail(client.user.avatarURL())
       .setTimestamp();
