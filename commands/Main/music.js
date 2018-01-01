0f07bd52188dc771befa5b2183fc9b3d74c47de5
Striker <zzstrikerzzyt@gmail.com>
2018-01-01 15:19:53 +0000

    Updated using async await

--- commands/Main/music.js
+++ commands/Main/music.js
@@ -1,25 +1,24 @@
 const Discord = require('discord.js');
 const yt = require('ytdl-core');
 
-exports.run = (client, message, args) => {
+exports.run = async(client, message, args) => {
     const [musicCommand, song] = args.join(' ').split(', ');
     if (musicCommand) {
         if (musicCommand == 'play') {
+            if (!song) return msg.reply('Please input a song!');
             const voiceChannel = message.member.voiceChannel;
             if (!voiceChannel) {
                 return message.reply('Please be in a voice channel first!');
             }
-            voiceChannel.join()
-                .then(connnection => {
-                    const stream = yt(song, {
-                        audioonly: true
-                    });
-                    const dispatcher = connnection.playStream(stream);
-                    dispatcher.on('end', () => {
-                        voiceChannel.leave();
-                    });
-                    dispatcher.setVolumeLogarithmic(5 / 5);
-                });
+            const connection = await voiceChannel.join();
+            const stream = yt(song, {
+                audioonly: true
+            });
+            const dispatcher = connection.playStream(stream);
+            dispatcher.on('end', () => {
+                voiceChannel.leave();
+            });
+            dispatcher.setVolumeLogarithmic(5 / 5);
         }
     } else return msg.reply('You must specify the music command you wish to use!');
 };
