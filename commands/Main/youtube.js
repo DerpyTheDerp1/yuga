86d5109064bb1363fb71f92a775b57b5bd9b62f0
Striker <zzstrikerzzyt@gmail.com>
2018-01-01 19:27:34 +0000

    Added youtube command

---
+++ commands/Main/youtube.js
@@ -0,0 +1,38 @@
+const search = require('youtube-search');
+const Discord = require('discord.js');
+
+const opts = {
+    maxResults: 1,
+    key: process.env.YTKEY
+};
+
+exports.run = (client, msg, args) => {
+    const searchTerm = args.join(' ');
+    if (!searchTerm) return msg.reply('Must specify a search term!');
+    else {
+        search(searchTerm, opts, (err, result) => {
+            if (err) msg.reply(`An error ocurred!\n\n\`\`\`${err.message}\`\`\``);
+            const video = result[0];
+            const resultEmbed = new Discord.MessageEmbed()
+                .setAuthor(`[${video.channelTitle}](https://www.youtube.com/channel/${encodeURIComponent(video.channelId)})`)
+                .setTitle(video.title)
+                .setThumbnail(video.thumbnails.default.url)
+                .setDescription(video.description)
+                .setTimestamp(video.publishedAt)
+                .setURL(video.link);
+            msg.channel.send({
+                embed: resultEmbed
+            });
+        });
+    }
+};
+
+exports.help = {
+    'help': {
+        name: 'Youtube',
+        description: 'Searches YouTube for the first resulting video.',
+        category: 'Main',
+        usage: 'y!youtube <search term>',
+        requiredPerms: 'None'
+    }
+}
