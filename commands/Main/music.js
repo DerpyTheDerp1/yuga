c33da529e0c179fc937123a07d01d5b32824fefe
Striker <zzstrikerzzyt@gmail.com>
2018-01-01 15:03:08 +0000

    Add music command

---
+++ commands/Main/music.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const yt = require('ytdl-core');
+
+exports.run = (client, message, args) => {
+    const [musicCommand, song] = args.join(' ').split(', ');
+    if (musicCommand) {
+        if (musicCommand == 'play') {
+            const voiceChannel = message.member.voiceChannel;
+            if (!voiceChannel) {
+                return message.reply('Please be in a voice channel first!');
+            }
+            voiceChannel.join()
+                .then(connnection => {
+                    const stream = yt(song, {
+                        audioonly: true
+                    });
+                    const dispatcher = connnection.playStream(stream);
+                    dispatcher.on('end', () => {
+                        voiceChannel.leave();
+                    });
+                });
+        }
+    } else return msg.reply('You must specify the music command you wish to use!');
+};
