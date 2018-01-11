9aa637c9f4ebe624b9658debb1f1e652dc5b52b8
Mr.Table <Limegreenxd@gmail.com>
2018-01-11 12:01:13 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -7,7 +7,9 @@ let command = '';
 exports.run = async(client, msg) => {
     if (client.user.username == 'Yuga!') return prefix == 'y!';
     if (client.user.username == 'Yuga Testing') return prefix == 'yt!';
-
+    if((msg.content.startsWith('y!')) && (msg.content.startsWith(`<@${client.user.id}>`)) && (!msg.content.startsWith('yt!'))){
+    return;
+    }
     let args = msg.content.split(' ').slice(1);
     command = msg.content.split(' ')[0];
     //Checking if it's a bot speaking & ignores it
