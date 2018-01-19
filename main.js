458f50341d0d4da913556b9b02aaee361b2fc8ad
Striker <zzstrikerzzyt@gmail.com>
2018-01-19 20:39:31 +0000

    Done, everything should be good

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client({
   disableEveryone: true
 });
-const { eventLoader } = require('./functions/index.js');
+const fs = require('fs');
 
-//Event Handler
-eventLoader();
+fs.readdir('./events', (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const eventFunction = require(`./events/${file}`);
+    const eventName = file.split('.')[0];
+    client.on(eventName, (...args) => eventFunction.run(client, ...args));
+  });
+});
 
 client.login(process.env.TOKEN);
