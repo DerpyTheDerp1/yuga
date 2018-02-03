e56912b128a4acfc40deb07a9dee13d1afbe9753
Striker <zzstrikerzzyt@gmail.com>
2018-02-03 21:07:31 +0000

    Borf

--- commands/Fun/dog.js
+++ commands/Fun/dog.js
@@ -1,17 +1,17 @@
 const Discord = require('discord.js');
 const snekfetch = require('snekfetch');
 
-exports.run = async (client, msg) => {
-   let res = await snekfetch.get('http://random.dog/woof.json');
-        while (res.body.file.includes('.mp4') || res.body.file.includes('.gif')) {
-           res = await snekfetch.get('http://random.dog/woof.json');
-        }
-        const embed = new Discord.MessageEmbed()
-            .setImage(res.body.file);
-        msg.channel.send({
-            embed
-        });
-    };
+exports.run = async(client, msg) => {
+    let res = await snekfetch.get('http://random.dog/woof.json');
+    while (res.body.url.includes('.mp4') || res.body.url.includes('.gif')) {
+        res = await snekfetch.get('http://random.dog/woof.json');
+    }
+    const embed = new Discord.MessageEmbed()
+        .setImage(res.body.url);
+    msg.channel.send({
+        embed
+    });
+};
 
 exports.help = {
     'help': {
