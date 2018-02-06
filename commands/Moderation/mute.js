a82386b0a014dafef6c65922c20bceefddac8ef0
Striker <zzstrikerzzyt@gmail.com>
2018-02-06 19:27:09 +0000

    Mute command added

---
+++ commands/Moderation/mute.js
@@ -0,0 +1,21 @@
+exports.run = (client, msg) => {
+    const user = msg.mentions.users.first();
+    const guild = msg.guild;
+    let mutedRole = guild.roles.find('name', 'Muted')
+    if (!mutedRole) mutedRole = guild.roles.create({
+        data: {
+            name: 'Muted',
+            color: 'BLACK',
+        },
+        reason: 'Muted role non existent',
+    })
+    guild.channels.filter(c => !c.permissionOverwrites.exists('id', mutedRole.id)).forEach(channels => {
+        channels.overwritePermissions(mutedRole.id, {
+            SEND_MESSAGES: false
+        })
+    });
+
+    user.member.roles.add(mutedRole.id)
+    msg.delete()
+    msg.reply(`Muted ${user} successfully.`)
+}
