1659ccc420e1e902ee15941efe00dc5d8dc57021
Striker <zzstrikerzzyt@gmail.com>
2018-02-14 17:12:44 +0000

    Update avatar.js

--- commands/Utility/avatar.js
+++ commands/Utility/avatar.js
@@ -1,43 +1,28 @@
-exports.run = (client, msg, args) => {
-  const Discord = require('discord.js');
-  const avhelp = new Discord.MessageEmbed()
-    .setTitle('Avatar Usage')
-    .setAuthor('Yuga')
-    .setColor(0x32CD32)
-    .addField('About', 'Returns the avatar of a specified user', false)
-    .addField('Usage', 'y!avatar <user mention>', false)
-    .addField('Perms required', 'None')
-    .setThumbnail(client.user.avatarURL())
-    .setTimestamp();
+const { MessageEmbed } = require('discord.js');
 
-  const userr = args.join(' ');
+exports.run = (client, msg) => {
+    const user = msg.mentions.users.first();
+    const avatar = user.avatarURL({
+        size: 2048
+    });
 
-  if (!userr) return msg.channel.send({
-    embed: avhelp
-  });
-  const user = msg.mentions.users.first();
-  const avatar = user.avatarURL({
-    size: 2048
-  });
+    const avatarembed = new MessageEmbed()
+        .setTitle(`Avatar of ${user.tag} (click here to get the avatar)`)
+        .setURL(avatar)
+        .setImage(avatar)
+        .setTimestamp();
 
-  const avatarembed = new Discord.MessageEmbed()
-    .setTitle(`Avatar of ${user.tag} (click here to get the avatar)`)
-    .setURL(avatar)
-    .setImage(avatar)
-    .setTimestamp();
-
-
-  msg.channel.send({
-    embed: avatarembed
-  });
+    msg.channel.send({
+        embed: avatarembed
+    });
 };
 
 exports.help = {
-  'help': {
-    name: 'Avatar',
-    description: 'Returns the avatar of a specified user',
-    category: 'Utility',
-    usage: 'y!avatar <tag user>',
-    requiredPerms: 'None'
-  }
-}
+    'help': {
+        name: 'Avatar',
+        description: 'Returns the avatar of a specified user',
+        category: 'Utility',
+        usage: 'y!avatar <tag user>',
+        requiredPerms: 'None'
+    }
+};
