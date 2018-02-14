b621ba68d4541fd84d1de2400f8b88ac8e07a775
Striker <zzstrikerzzyt@gmail.com>
2018-02-14 18:31:32 +0000

    Placeholder

---
+++ commands/Fun/kiss.js
@@ -0,0 +1,1 @@
+exports.run = (client, msg) => msg.channel.send('Kiss requires GIFs to function. Please consider contributing GIFs to Yuga by viewing announcements in the official Striker\'s Coding Cave server');
