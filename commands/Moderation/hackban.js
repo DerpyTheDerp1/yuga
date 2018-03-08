a1a915b5c623f6a3d4907d2600091533b64e701a
Striker <zzstrikerzzyt@gmail.com>
2018-03-08 20:18:25 +0000

    Hackbaaaan

---
+++ commands/Moderation/hackban.js
@@ -0,0 +1,41 @@
+exports.run = async (client, msg) => {
+    const guild = msg.guild;
+    const user = msg.mentions.users.first();
+    const userID = msg.mentions.users.first().id;
+    const Discord = require('discord.js');
+    const denied = new Discord.MessageEmbed()
+        .setTitle('ACCESS DENIED')
+        .setAuthor(msg.author.tag)
+        .setColor(0xFF0000)
+        .setDescription('You do not have the permissions needed to use this command. Missing perms: BAN_MEMBERS')
+        .setThumbnail(client.user.avatarURL())
+        .setTimestamp();
+
+    if (!msg.mentions.users.first()) return msg.reply('Must specify a user!');
+
+    const hasban = 'BAN_MEMBERS';
+    if (msg.member.hasPermission(hasban)) {
+        const banMessage = await msg.channel.send('Banning user...');
+        guild.member(user).ban();
+        await banMessage.edit('Banned and messages deleted (past 7 days)\nUnbannning');
+        await banMessage.edit('Banned and messages deleted (past 7 days)\nUnbannning.');
+        await banMessage.edit('Banned and messages deleted (past 7 days)\nUnbannning..');
+        guild.members.unban(userID);
+        await banMessage.edit('Banned and messages deleted (past 7 days)\nUnbannning...');
+        await banMessage.edit('Banned and messages deleted (past 7 days)\nUnbanned!');
+    } else {
+        msg.channel.send({
+            embed: denied,
+        });
+    }
+};
+
+exports.help = {
+    'help': {
+        name: 'Ban',
+        description: 'Bans a member',
+        category: 'Moderation',
+        usage: 'y!ban <tag member>',
+        requiredPerms: 'Ban Members',
+    },
+};
