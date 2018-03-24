5edddfeb425fffd336e648fada5a1107e959a78e
Striker <zzstrikerzzyt@gmail.com>
2018-03-24 15:25:16 +0000

    update muting

--- commands/Moderation/mute.js
+++ commands/Moderation/mute.js
@@ -29,7 +29,9 @@ exports.run = (client, msg) => {
         SEND_MESSAGES: false,
       });
     });
-
+    guild.member(user).roles.forEach(role => {
+      guild.member(user).roles.remove(role);
+    });
     guild.member(user).roles.add(mutedRole);
     msg.delete();
     msg.channel.send(`Muted ${user} successfully.`);
