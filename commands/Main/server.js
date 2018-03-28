3a397d52ec85bd7999c20dd9d2b5b99ca712a4de
Striker <zzstrikerzzyt@gmail.com>
2018-03-28 17:32:02 +0100

    Changed some shit

--- commands/Main/server.js
+++ commands/Main/server.js
@@ -1,14 +1,1 @@
-exports.run = (client, msg) => {
-  const server = '308278703283240960';
-  const guild = client.guilds.get(server);
-
-  const channel = guild.channels.filter(c => c.permissionsFor(guild.me).has('SEND_MESSAGES')).first();
-
-
-  channel.createInvite({
-    maxAge: 0,
-  })
-    .then((invite) => {
-      msg.channel.send(`Here is an invite for the official server (Striker's Coding Den) | ${invite}`);
-    });
-};
+exports.run = (client, msg) => msg.channel.send('Here is an invite for Striker\'s Weeb Corner, where you can chat etc.\nYuga is based here, so you can also get support in here.')
