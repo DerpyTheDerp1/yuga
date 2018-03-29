341afd519cf323ca1e3520d9e1abfee7660e101d
Striker <zzstrikerzzyt@gmail.com>
2018-03-29 15:49:32 +0100

    server info is dank

--- commands/Utility/serverinfo.js
+++ commands/Utility/serverinfo.js
@@ -2,48 +2,6 @@ const Discord = require('discord.js');
 const { color } = require('../../db/db.js');
 
 exports.run = (client, msg) => {
-  const roles = [];
-  msg.guild.roles.forEach((r) => {
-    roles.push(r.name);
-  });
-
-  const channels = [];
-  msg.guild.channels.forEach((c) => {
-    if (c.type == 'text') {
-      channels.push(c.name + ' (text)');
-    }
-    if (c.type == 'voice') {
-      channels.push(c.name + '(voice)');
-    }
-  });
-
-  let botCount = 0;
-  msg.guild.members.forEach((m) => {
-    if (m.user.bot) {
-      botCount++;
-    }
-  });
-
-  let userCount = 0;
-  msg.guild.members.forEach((m) => {
-    if (!m.user.bot) {
-      userCount++;
-    }
-  });
-
-  let verificationLevel = msg.guild.verificationLevel;
-  const indexes = ['0', '1', '2', '3', '4'];
-  const levels = [
-    'None',
-    'Low',
-    'Medium',
-    '(╯°□°）╯︵ ┻━┻ ',
-    '┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻',
-  ];
-  for (const i in indexes) {
-    verificationLevel = levels[i];
-  }
-
   const serverinfo = new Discord.MessageEmbed()
     .setTitle(`Server info for ${msg.guild.name}`)
     .setThumbnail(client.user.avatarURL())
@@ -53,12 +11,10 @@ exports.run = (client, msg) => {
     .setColor(color)
     .addField('Owner', `${msg.guild.owner} ID: ${msg.guild.owner.id}`)
     .addField('Created at', msg.guild.createAt, true)
-    .addField('Channels', channels, true)
-    .addField('Roles', roles, true)
-    .addField('Users', userCount, true)
-    .addField('Bots', botCount, true)
-    .addField('Verification level', verificationLevel, true);
-
+    .addField('Channels', msg.guild.channels.size, true)
+    .addField('Roles', msg.guild.roles.size, true)
+    .addField('Users', msg.guild.members.filter(m => !m.user.bot).size, true)
+    .addField('Total users', msg.guild.members.size, true);
   msg.channel.send({
     embed: serverinfo,
   });
