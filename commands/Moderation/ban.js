23a5049130c493a325af2d7c3d982f006221ca82
Striker <zzstrikerzzyt@gmail.com>
2018-04-08 19:18:50 +0100

    Fixed something

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -1,23 +1,14 @@
 exports.run = (client, msg) => {
-  const Discord = require('discord.js');
-  const denied = new Discord.MessageEmbed()
-    .setTitle('ACCESS DENIED')
-    .setAuthor(msg.author.tag)
-    .setColor(0xFF0000)
-    .setDescription('You do not have the permissions needed to use this command. Missing perms: BAN_MEMBERS')
-    .setThumbnail(client.user.avatarURL())
-    .setTimestamp();
-
   if (!msg.mentions.users.first()) return msg.reply('Must specify a user!');
 
   const hasban = 'BAN_MEMBERS';
   if (msg.member.hasPermission(hasban)) {
+    if (msg.member.roles.highest.comparePositionTo(msg.guild.member(msg.mentions.users.first())) < 0) return msg.reply('You cannot ban this user, they have a higher role than you!');
+    if (msg.member.roles.highest.comparePositionTo(msg.guild.member(msg.mentions.users.first())) == 0) return msg.reply('You cannot ban this user, they the same role than you!');
     msg.guild.member(msg.mentions.users.first()).ban();
-    msg.reply(`The ban hammer :hammer: has struck on **${msg.mentions.users.first().username}**!`);
+    msg.channel.send(`The ban hammer :hammer: has struck on **${msg.mentions.users.first().username}**!`);
   } else {
-    msg.channel.send({
-      embed: denied,
-    });
+    msg.channel.send('You cannot ban this user, you do not have the permissions to!');
   }
 };
 
--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -1,23 +1,14 @@
-const Discord = require('discord.js');
-
 exports.run = (client, msg) => {
-  const denied = new Discord.MessageEmbed()
-    .setTitle('ACCESS DENIED')
-    .setAuthor(msg.author.tag)
-    .setColor(0xFF0000)
-    .setDescription('You do not have the permissions needed to use this command. Missing perms: KICK_MEMBERS')
-    .setThumbnail(client.user.avatarURL())
-    .setTimestamp();
   const haskick = 'KICK_MEMBERS';
 
   if (!msg.mentions.users.first()) return msg.reply('Must specify a user!');
   if (msg.member.hasPermission(haskick)) {
+    if (msg.member.roles.highest.comparePositionTo(msg.guild.member(msg.mentions.users.first())) < 0) return msg.reply('You cannot kick this user, they have a higher role than you!');
+    if (msg.member.roles.highest.comparePositionTo(msg.guild.member(msg.mentions.users.first())) == 0) return msg.reply('You cannot kick this user, they the same role than you!');
     msg.guild.member(msg.mentions.users.first()).kick();
     msg.channel.send(`User **${msg.mentions.users.first().username}** kicked!`);
   } else {
-    msg.channel.send({
-      embed: denied,
-    });
+    msg.reply('You cannot kick this user, you don\'t have the permissions to.');
   }
 };
 
