238bdca7ebc411b3628f6f792f5313b1e6908bce
Striker <zzstrikerzzyt@gmail.com>
2018-04-08 19:28:51 +0100

    Fixed again

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -1,23 +1,23 @@
 exports.run = (client, msg) => {
-  if (!msg.mentions.users.first()) return msg.reply('Must specify a user!');
-
-  const hasban = 'BAN_MEMBERS';
-  if (msg.member.hasPermission(hasban)) {
-    if (msg.member.roles.highest.comparePositionTo(msg.guild.member(msg.mentions.users.first())) < 0) return msg.reply('You cannot ban this user, they have a higher role than you!');
-    if (msg.member.roles.highest.comparePositionTo(msg.guild.member(msg.mentions.users.first())) == 0) return msg.reply('You cannot ban this user, they the same role than you!');
-    msg.guild.member(msg.mentions.users.first()).ban();
-    msg.channel.send(`The ban hammer :hammer: has struck on **${msg.mentions.users.first().username}**!`);
-  } else {
-    msg.channel.send('You cannot ban this user, you do not have the permissions to!');
-  }
+    if (!msg.mentions.users.first()) return msg.reply('Must specify a user!');
+    const member = msg.guild.member(msg.mentions.users.first());
+    const hasban = 'BAN_MEMBERS';
+    if (msg.member.hasPermission(hasban)) {
+        if (msg.member.roles.highest.position < member.roles.highest.position) return msg.reply('You cannot kick this user, they have a higher role than you!');
+        if (msg.member.roles.highest.position == member.roles.highest.position) return msg.reply('You cannot kick this user, they the same role than you!');
+        member.ban();
+        msg.channel.send(`The ban hammer :hammer: has struck on **${msg.mentions.users.first().username}**!`);
+    } else {
+        msg.channel.send('You cannot ban this user, you do not have the permissions to!');
+    }
 };
 
 exports.help = {
-  'help': {
-    name: 'Ban',
-    description: 'Bans a member',
-    category: 'Moderation',
-    usage: 'y!ban <tag member>',
-    requiredPerms: 'Ban Members',
-  },
+    'help': {
+        name: 'Ban',
+        description: 'Bans a member',
+        category: 'Moderation',
+        usage: 'y!ban <tag member>',
+        requiredPerms: 'Ban Members',
+    },
 };
--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -1,11 +1,11 @@
 exports.run = (client, msg) => {
   const haskick = 'KICK_MEMBERS';
-
+  const member = msg.guild.member(msg.mentions.users.first());
   if (!msg.mentions.users.first()) return msg.reply('Must specify a user!');
   if (msg.member.hasPermission(haskick)) {
-    if (msg.member.roles.highest.comparePositionTo(msg.guild.member(msg.mentions.users.first())) < 0) return msg.reply('You cannot kick this user, they have a higher role than you!');
-    if (msg.member.roles.highest.comparePositionTo(msg.guild.member(msg.mentions.users.first())) == 0) return msg.reply('You cannot kick this user, they the same role than you!');
-    msg.guild.member(msg.mentions.users.first()).kick();
+    if (msg.member.roles.highest.position < member.roles.highest.position) return msg.reply('You cannot kick this user, they have a higher role than you!');
+    if (msg.member.roles.highest.position == member.roles.highest.position) return msg.reply('You cannot kick this user, they the same role than you!');
+    member.kick();
     msg.channel.send(`User **${msg.mentions.users.first().username}** kicked!`);
   } else {
     msg.reply('You cannot kick this user, you don\'t have the permissions to.');
