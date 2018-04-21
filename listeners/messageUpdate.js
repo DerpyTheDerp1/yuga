a96601ac13aefd6a2dc1bf13a41fb71cc566f063
Striker <zzstrikerzzyt@gmail.com>
2018-04-21 14:43:22 +0100

    Got rid of Yuga logging 9000

--- listeners/messageUpdate.js
+++ listeners/messageUpdate.js
@@ -10,20 +10,22 @@ class messageUpdateListener extends Listener {
     }
 
     exec(msg) {
-        if (msg.guild.id == '264445053596991498') return;
-        if (msg.author.bot) return;
-        msg.channel.startTyping();
-        const log = this.client.util.embed()
-            .setTitle('**__LOG__**')
-            .setColor(color)
-            .addField('User', `${msg.author.tag} ID: ${msg.author.id}`)
-            .addField('Command', `${msg.content}`)
-            .addField('Server', `${msg.guild.name} ID: ${msg.guild.id}`)
-            .setTimestamp()
-            .setThumbnail(this.client.user.avatarURL());
-        this.client.channels.get('428587939958882304').send({
-            embed: log,
-        }).then(() => msg.channel.stopTyping(true));
+        if (msg.content.startsWith('y!')) {
+            if (msg.guild.id == '264445053596991498') return;
+            if (msg.author.bot) return;
+            msg.channel.startTyping();
+            const log = this.client.util.embed()
+                .setTitle('**__LOG__**')
+                .setColor(color)
+                .addField('User', `${msg.author.tag} ID: ${msg.author.id}`)
+                .addField('Command', `${msg.content}`)
+                .addField('Server', `${msg.guild.name} ID: ${msg.guild.id}`)
+                .setTimestamp()
+                .setThumbnail(this.client.user.avatarURL());
+            this.client.channels.get('428587939958882304').send({
+                embed: log,
+            }).then(() => msg.channel.stopTyping(true));
+        }
     }
 }
 
