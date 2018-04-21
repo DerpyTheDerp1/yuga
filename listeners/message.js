b84c8965db3f4bdb0f3adc105288286cceaec3bd
Striker <zzstrikerzzyt@gmail.com>
2018-04-21 18:42:50 +0100

    Added this smart thing

--- listeners/message.js
+++ listeners/message.js
@@ -10,21 +10,20 @@ class MessageListener extends Listener {
     }
 
     exec(msg) {
-        if (msg.content.startsWith('y!')) {
+        if (msg.content.startsWith(this.client.commandHandler.prefix)) {
             if (msg.guild.id == '264445053596991498') return;
             if (msg.author.bot) return;
-            msg.channel.startTyping();
             const log = this.client.util.embed()
                 .setTitle('**__LOG__**')
                 .setColor(color)
                 .addField('User', `${msg.author.tag} ID: ${msg.author.id}`)
                 .addField('Command', `${msg.content}`)
-                .addField('Server', `${msg.guild.name} ID: ${msg.guild.id}`)
                 .setTimestamp()
                 .setThumbnail(this.client.user.avatarURL());
+            msg.guild ? log.addField('Server', `${msg.guild.name} ID: ${msg.guild.id}`) : log.addField('Used In DMS', '^');
             this.client.channels.get('428587939958882304').send({
                 embed: log,
-            }).then(() => msg.channel.stopTyping(true));
+            });
         }
     }
 }
--- listeners/messageUpdate.js
+++ listeners/messageUpdate.js
@@ -10,7 +10,7 @@ class messageUpdateListener extends Listener {
     }
 
     exec(msg) {
-        if (msg.content.startsWith('y!')) {
+        if (msg.content.startsWith(this.client.commandHandler.prefix)) {
             if (msg.guild.id == '264445053596991498') return;
             if (msg.author.bot) return;
             msg.channel.startTyping();
@@ -19,12 +19,12 @@ class messageUpdateListener extends Listener {
                 .setColor(color)
                 .addField('User', `${msg.author.tag} ID: ${msg.author.id}`)
                 .addField('Command', `${msg.content}`)
-                .addField('Server', `${msg.guild.name} ID: ${msg.guild.id}`)
                 .setTimestamp()
                 .setThumbnail(this.client.user.avatarURL());
+            msg.guild ? log.addField('Server', `${msg.guild.name} ID: ${msg.guild.id}`) : log.addField('Used In DMS', '^');
             this.client.channels.get('428587939958882304').send({
                 embed: log,
-            }).then(() => msg.channel.stopTyping(true));
+            });
         }
     }
 }
