0973751e84e9879cb8ecc75b32a459a9e44fa3be
Striker <zzstrikerzzyt@gmail.com>
2018-04-24 16:03:28 +0100

    NOW FIXED

--- commands/Main/ping.js
+++ commands/Main/ping.js
@@ -32,7 +32,7 @@ class PingCommand extends Command {
             roundedPing = ping / 1000;
         let Os = 'o',
             Is = 'i';
-        for (let x=0; x < Math.round((roundedPing / 2) / 2);) {
+        for (let x=0; x < Math.round((roundedPing / 2) / 2); x++) {
             Os+='o';
             Is+='i';
         }
